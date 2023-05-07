ea76e8438857a6de4365176cada201a50b88fe4c
korayinal <korayinal93@gmail.com>
2023-05-07 14:06:38 +0200

    First

---
+++ Coding Exercises/Assignment 1/Assignment1/Assignment1/IFC_Application.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Linq;
+using GeometryGym.Ifc;
+
+namespace GettingStarted
+{
+    internal class IFC_Application
+    {
+        static void Main(string[] args)
+        {
+            //create new IFC Model
+            var db = new DatabaseIfc(ModelView.Ifc2x3Coordination);
+            var project = new IfcProject(db, "my first IFC project");
+
+            // access all entity instances stored in the IFC Model
+            var allEntities = db.ToList();
+
+            // access an show an instance
+            foreach (var entity in allEntities)
+            {
+                Console.WriteLine(entity);
+            }
+
+            // await user input
+            Console.ReadKey();
+
+        }
+    }
+}
